53457c8184a584336c135efdea54c16f5dadf438
Luciano Ramalho <luciano@ramalho.org>
2018-09-21 13:48:05 -0300

    trabalhando no exemplo com Unicode Regional Indicator

---
+++ bandeira/bandeira.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# https://tapoueh.org/blog/2017/07/playing-with-unicode/
+
+codigo_B = 0x1F1E7
+codigo_R = 0x1F1F7
+
+# não funcionou no terminal do Ubuntu 18.04
+print(chr(codigo_B) + chr(codigo_R))
