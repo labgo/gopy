c8d6e163bf5d093d7751a5044e50f84b0c911779
Luciano Ramalho <luciano@ramalho.org>
2018-09-24 20:12:24 -0300

    palavras mais comuns em nomes Unicode

---
+++ exemplos/strings/uninames/words/main.go
@@ -0,0 +1,37 @@
+// words counts all words that appear in Unicode character names
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+
+	"golang.org/x/text/unicode/runenames"
+)
+
+const lastCodePoint = 0x10FFFF // http://unicode.org/faq/utf_bom.html
+
+func main() {
+	fmt.Println("Unicode version", runenames.UnicodeVersion)
+	var r rune
+	counts := make(map[string]int)
+	for r = 0; r <= lastCodePoint; r++ {
+		name := runenames.Name(r)
+		if len(name) == 0 || name[0] == '<' {
+			continue // ignore nameless and named ranges
+		}
+		words := strings.Fields(name)
+		for _, w := range words {
+			counts[w]++
+		}
+	}
+	report := []string{}
+	for line, n := range counts {
+		if n > 1 {
+			report = append(report, fmt.Sprintf("%6d\t%s", n, line))
+		}
+	}
+	// sort.Strings(report)  // ascending order
+	sort.Sort(sort.Reverse(sort.StringSlice(report))) // descending order
+	fmt.Println(strings.Join(report, "\n"))
+}
